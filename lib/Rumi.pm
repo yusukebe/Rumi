cc00e80b343d84eeadcbcb30f198d733db963e95
Yusuke Wada <yusukebe@cpan.org>
2011-01-19 10:47:47 +0900

    initial commit

---
+++ lib/Rumi.pm
@@ -0,0 +1,32 @@
+package Rumi;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Rumi -
+
+=head1 SYNOPSIS
+
+  use Rumi;
+
+=head1 DESCRIPTION
+
+Rumi is
+
+=head1 AUTHOR
+
+Yusuke Wada E<lt>yusuke at kamawada.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
